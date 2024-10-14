2b67cf0b68a4c1444f469602da88edc9740aba9f
Anurag-dixit123 <abhidixit9536@gmail.com>
2024-10-14 12:37:23 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation(){
+ 
 
 var tl = gsap.timeline();
 
@@ -29,16 +30,46 @@ tl.to(".line h2", {
 })
 tl.to("#loader", {
     opacity:0,
-    delay:4,
+    delay:2,
     duration:0.2,
 })
 
 tl.from("#page1", {
     delay:0.2,
     y:1600,
-    opacity:0,
+   duration:0.6,
     ease:Power4,
 })
 tl.to("#loader", {
-    display:none,
+ display:"none",
 })
+tl.from("#nav",{
+    opacity:0,
+ })
+ 
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+    y:120,
+    stagger:0.2,
+})
+
+
+
+}
+
+loadingAnimation()
+
+
+function cursorAnimation() {
+    
+document.addEventListener('mousemove', function (dets){
+    
+    gsap.to('#crsr',{
+     left:dets.x,
+     top:dets.y,
+    })
+ })
+ 
+ Shery.makeMagnet(".nav #nav-part2 h4");
+}
+
+cursorAnimation()
